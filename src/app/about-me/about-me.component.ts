5c010abcb70b0d237475ada9edbe4d3355d10833
alexsanro1 <alexsanro1@gmail.com>
2020-05-28 23:05:18 +0200

    Control displayed of div contacts with angular

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss'],
+  host: {
+    '(window:resize)': 'onResize($event)'
+  }
 })
 export class AboutMeComponent implements OnInit {
 
   constructor() { }
+  displayContactDiv: boolean = true;
 
   ngOnInit(): void {
+    this.displayContact(window.innerWidth) 
+  }
+  onResize(event){
+    var windowWidth = event.target.innerWidth;
+    this.displayContact(windowWidth);
   }
 
+  displayContact(size: Number){
+    if(size < 992){
+      this.displayContactDiv = false;
+    }else{
+      this.displayContactDiv = true;
+    }  
+  }
 }
